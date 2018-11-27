6a0a76d78320ab0fdf67bc7a7997bd87d5a050c1
baptistedufour <bdufour@enseirb-matmeca.fr>
2018-11-27 14:56:16 +0100

    version qui compile avec eigen removed

--- LevelSet.cpp
+++ LevelSet.cpp
@@ -7,34 +7,33 @@
  * redistancing max_iter times
  *
  */
-
 void LevelSet::redistancing(const int max_iter)
 {
-  std::vector<std::vector<double>> phi0_v = _phi_v;
-  std::vector<std::vector<double>> phin_v = _phi_v;
+    field phi0 = _phi;
+    field phin = _phi;
 
     int iter = 0, pourcentage =0;
 
     while (iter<max_iter)
       {
-        firstOrderUpwind_v(_phi_v, phi0_v, .5*1);
-        phin_v = _phi_v;
+        firstOrderUpwind(_phi, phi0, .5*1);
+        phin = _phi;
 
-      	if ((floor(iter*100/max_iter)>=pourcentage)&&(max_iter>20))
-      	  {
-      	    pourcentage=pourcentage+1;
+	if ((floor(iter*100/max_iter)>=pourcentage)&&(max_iter>20))
+	  {
+	    pourcentage=pourcentage+1;
 
-      	    int i_barre;
-      	    printf( "[" );
-      	    for (i_barre = 0 ; i_barre <= pourcentage ; i_barre += 2) printf( "*" );
-      	    for (; i_barre <= 100 ; i_barre += 2 ) printf( "-" );
-      	    printf( "] %3d %%", pourcentage );
+	    int i_barre;
+	    printf( "[" );
+	    for(i_barre=0;i_barre<=pourcentage;i_barre+=2) printf( "*" );
+	    for (;i_barre<=100; i_barre+=2 ) printf( "-" );
+	    printf( "] %3d %%", pourcentage );
 
-      	    for(i_barre=0;i_barre<59;++i_barre) printf( "%c", 8 );
+	    for(i_barre=0;i_barre<59;++i_barre) printf( "%c", 8 );
 
-      	    fflush(stdout );
+	    fflush(stdout );
 
-      	  }
+	  }
 
         iter++;
       }
@@ -46,51 +45,19 @@ void LevelSet::redistancing(const int max_iter)
  * redistancing until equilibrium
  *
  */
- void LevelSet::redistancing(const double eps)
- {
-     field phi0 = _phi;
-     field phin = _phi;
-
-     int iter = 0; double err = 2*eps;
-     while (err>eps)
-     {
-         firstOrderUpwind(_phi, phi0, .5*1);
-         err = 2*(phin-_phi).abs().maxCoeff()/1;
-         phin = _phi;
-         iter++;
-     }
- }
-
-/*
- * first order upwind_v
- *
- */
-void LevelSet::firstOrderUpwind_v(std::vector<std::vector<double>>& phi_v, const std::vector<std::vector<double>>& phi0_v, const double dt)
+void LevelSet::redistancing(const double eps)
 {
-    std::vector<std::vector<double>> dxm = diff_m_v(phi_v, 0),
-                                     dxp = diff_p_v(phi_v, 0),
-                                     dym = diff_m_v(phi_v, 1),
-                                     dyp = diff_p_v(phi_v, 1);
+    field phi0 = _phi;
+    field phin = _phi;
 
-    std::vector<std::vector<double>> G = godunov_v(dxm, dxp, dym, dyp);
-    std::vector<std::vector<double>> phi1_v = phi_v;
-
-    for (int  i=0; i<phi_v.size(); ++i)
-        for (int  j=0; j<phi_v[0].size(); ++j)
-        {
-            int mini(i+1); if (phi_v.size()-1 < mini) {mini = phi_v.size()-1;}
-            int minj(j+1); if (phi_v[0].size()-1 < minj) {minj = phi_v[0].size()-1;}
-
-            double  pij = phi0_v[i][j],
-                    pimj = phi0_v[std::max(i-1,0)][j], pipj = phi0_v[mini][j],
-                    pijm = phi0_v[i][std::max(j-1,0)], pijp = phi0_v[i][minj];
-            double delta = boundary(pij, pimj, pipj, pijm, pijp);
-            double Dij = .5*sgn(pij)*1; // Only for masks !
-
-            phi1_v[i][j] = phi_v[i][j]-dt*delta*sgn(phi0_v[i][j])*G[i][j];
-            phi1_v[i][j]-= dt*(1-delta)*( sgn(phi0_v[i][j])*fabs(phi_v[i][j])-Dij )/1;
-        }
-    phi_v = phi1_v;
+    int iter = 0; double err = 2*eps;
+    while (err>eps)
+    {
+        firstOrderUpwind(_phi, phi0, .5*1);
+        err = 2*(phin-_phi).abs().maxCoeff()/1;
+        phin = _phi;
+        iter++;
+    }
 }
 
 /*
@@ -129,7 +96,6 @@ void LevelSet::firstOrderUpwind(field& phi, const field& phi0, const double dt)
  * check :
  *      retun |Grad(phi)|
  */
-
 field LevelSet::check() const
 {
     return (diff(_phi,0).square()+diff(_phi,1).square()).sqrt();
@@ -152,7 +118,6 @@ bool LevelSet::check(const double eps) const
  * normal:
  *      return Grad(phi)/|Grad(phi)|
  */
-
 std::vector<field> LevelSet::normal() const
 {
     std::vector<field> n(2);
@@ -184,34 +149,6 @@ field LevelSet::curvature() const
 }
 
 /*
- * Godunov()_v :
- *
- */
-std::vector<std::vector<double>> LevelSet::godunov_v(const std::vector<std::vector<double>>& xm, const std::vector<std::vector<double>>& xp, const std::vector<std::vector<double>>& ym, const std::vector<std::vector<double>>& yp) const
-{
-    // field result = pos(_phi)*( (xm.max(0.).max(-xp.min(0.))).pow(2) + (ym.max(0.).max(-yp.min(0.))).pow(2)).sqrt();
-    // result += neg(_phi)*( (xp.max(0.).max(-xm.min(0.))).pow(2) + (yp.max(0.).max(-ym.min(0.))).pow(2) ).sqrt();
-
-    std::vector<std::vector<double>> result;
-    result.resize(_phi.rows());
-    for (int i=0;i<_phi.rows() ;i++) { result[i].resize(_phi.cols()); }
-
-    double coef1( sqrt( pow(std::max( std::min(0.,-fmax(xp)), std::max(0., fmax(xm))), 2) + pow(std::max( std::min( 0., -fmin(yp)), std::max(0., fmax(ym))), 2)) );
-    double coef2( sqrt( pow(std::max( std::min(0.,-fmax(xm)), std::max(0., fmax(xp))), 2) + pow(std::max( std::min( 0., -fmax(ym)), std::max(0., fmax(yp))), 2)) );
-
-    for (int i; i<result.size(); i++)
-    {
-      for (int j; j<result[0].size(); j++)
-      {
-        result[i][j] = (pos_v(_phi_v,i,j) * coef1 + neg_v(_phi_v,i,j)*coef2) -1.;
-
-      }
-    }
-
-    return result ; //- 1;
-}
-
-/*
  * Godunov() :
  *
  */
@@ -221,7 +158,6 @@ field LevelSet::godunov(const field& xm, const field& xp, const field& ym, const
     result += neg(_phi)*( (xp.max(0.).max(-xm.min(0.))).pow(2) + (yp.max(0.).max(-ym.min(0.))).pow(2) ).sqrt();
     return result - 1;
 }
-
 
 /*
  * sgn(double X):
@@ -254,52 +190,10 @@ double LevelSet::boundary(const double ij, const double imj,  const double ipj, 
 {
     double eps = 1*1e-15;
 
-    double a = ( 1+std::min(ij*imj,0.0)/(fabs(ij*imj)+eps) )*( 1+std::min(ij*ipj,0.0)/(fabs(ij*ipj)+eps) );
-    double b = ( 1+std::min(ij*ijm,0.0)/(fabs(ij*ijm)+eps) )*( 1+std::min(ij*ijp,0.0)/(fabs(ij*ijp)+eps) );
+    double a = ( 1+fmin(ij*imj,0.0)/(fabs(ij*imj)+eps) )*( 1+fmin(ij*ipj,0.0)/(fabs(ij*ipj)+eps) );
+    double b = ( 1+fmin(ij*ijm,0.0)/(fabs(ij*ijm)+eps) )*( 1+fmin(ij*ijp,0.0)/(fabs(ij*ijp)+eps) );
 
     return a*b;
-}
-
-/*
- * pos(X)_v :
- *      return max(X,0)/(|X|+eps)
- */
-double LevelSet::pos_v(const std::vector<std::vector<double>>& X, int a, int b) const
-{
-    double max=std::max(fmax(X),0.);
-    std::vector<std::vector<double>> newX;
-    newX.resize(X.size());
-    for (int i=0;i<newX.size() ;i++) { newX[i].resize(X[0].size()); }
-
-    for (int i; i<newX.size(); i++)
-    {
-      for (int j; j<newX[0].size(); j++)
-      {
-        newX[i][j]= max/(abs(X[i][j])+1e-15);
-      }
-    }
-    return newX[a][b];
-}
-
-/*
- * neg(X)_v :
- *      return -min(X,0)/(|X|+eps)
- */
-double LevelSet::neg_v(const std::vector<std::vector<double>>& X, int a, int b) const
-{
-  double min=std::min(fmin(X),0.);
-  std::vector<std::vector<double>> newX;
-  newX.resize(X.size());
-  for (int i=0;i<newX.size() ;i++) { newX[i].resize(X[0].size()); }
-
-  for (int i; i<newX.size(); i++)
-  {
-    for (int j; j<newX[0].size(); j++)
-    {
-      newX[i][j]= min/(abs(X[i][j])+1e-15);
-    }
-  }
-  return newX[a][b];
 }
 
 /*
@@ -319,6 +213,7 @@ field LevelSet::neg(const field& X) const
 {
     return -X.min(0.0)/(X.abs()+1*1e-15);
 }
+
 
 field LevelSet::diff(const field &F, bool index) const
 {
@@ -346,193 +241,6 @@ field LevelSet::diff(const field &F, bool index) const
          3.0 * F.block(0, N2 - 1, N1, 1));
   }
   return diffF;
-}
-
-
-std::vector<std::vector<double>> LevelSet::diff_v(const std::vector<std::vector<double>> &F, bool index) const
-{
-  auto diffF = F;
-
-  for (int i=0; i<diffF.size(); i++)
-  {
-    for(int j=0; j<diffF[0].size(); j++ )
-    {
-      diffF[i][j] = 0.;
-    }
-  }
-  int N1(F.size()), N2(F[0].size()), d1(1.0), d2(1.0);
-  if (!index) { // x
-    //diffF.block(1, 0, N1 - 2, N2) =
-    //    0.5 / d1 * (F.block(2, 0, N1 - 2, N2) - F.block(0, 0, N1 - 2, N2));
-    for (int i=1; i<N1-1; i++)
-    {
-      for (int j=0; j<N2; j++)
-      {
-        diffF[i][j] = 0.5 / d1 * (F[i+1][j] - F[i-1][j]);
-      }
-    }
-    //diffF.block(0, 0, 1, N2) =
-    //    0.5 / d1 * (4.0 * F.block(1, 0, 1, N2) - F.block(2, 0, 1, N2) -
-    //                3.0 * F.block(0, 0, 1, N2));
-    for (int j=0; j<N2; j++)
-    {
-      diffF[0][j] = 0.5 / d1 * (4.0 * F[1][j] - F[2][j] - 3.0 * F[0][j]);
-    }
-    //diffF.block(N1 - 1, 0, 1, N2) =
-    //    0.5 / d1 *
-    //    (-4.0 * F.block(N1 - 2, 0, 1, N2) + F.block(N1 - 3, 0, 1, N2) +
-    //     3.0 * F.block(N1 - 1, 0, 1, N2));
-    for (int j=0; j<N2; j++)
-    {
-      diffF[N1-1][j] = 0.5 / d1 * (-4.0 * F[N1-2][j] + F[N1-3][j] + 3.0 * F[N1-1][j]);
-    }
-  } else { // y
-    //diffF.block(0, 1, N1, N2 - 2) =
-    //    0.5 / d2 * (F.block(0, 2, N1, N2 - 2) - F.block(0, 0, N1, N2 - 2));
-    for (int i=0; i<N1; i++)
-    {
-      for (int j=1; j<N2-1; j++)
-      {
-        diffF[i][j] = 0.5 / d2 * (F[i][j+1] - F[i][j-1]);
-      }
-    }
-    //diffF.block(0, 0, N1, 1) =
-    //    0.5 / d2 * (4.0 * F.block(0, 1, N1, 1) - F.block(0, 2, N1, 1) -
-    //                3.0 * F.block(0, 0, N1, 1));
-    for (int i=0; i<N1; i++)
-    {
-      diffF[i][0] = 0.5 / d2 * (4.0 * F[i][1] - F[i][2] - 3.0 * F[i][0]);
-    }
-    //diffF.block(0, N2 - 1, N1, 1) =
-    //    0.5 / d2 *
-    //    (-4.0 * F.block(0, N2 - 2, N1, 1) + F.block(0, N2 - 3, N1, 1) +
-    //     3.0 * F.block(0, N2 - 1, N1, 1));
-    for (int i=0; i<N1; i++)
-    {
-      diffF[i][N2-1] = 0.5 / d2 * (-4.0 * F[i][N2-2] + F[i][N2-3] + 3.0 * F[i][N2-1]);
-    }
-  }
-  return diffF;
-}
-
-/*
- * diff_m_v:
- *      dérivé à gauche (m <-> moins)
- *
- */
-std::vector<std::vector<double>> LevelSet::diff_m_v(const std::vector<std::vector<double>>& F, int index) const
-{
-    std::vector<std::vector<double>> diff_v = F;
-    for (int i=0; i<diff_v.size(); i++)
-    {
-      for(int j=0; j<diff_v[0].size(); j++ )
-      {
-        diff_v[i][j] = 0.;
-      }
-    }
-    for (int  i=0; i<F.size(); ++i)
-        for (int  j=0; j<F[0].size(); ++j)
-            diff_v[i][j] = diff_m_v(F, index, i, j);
-    return diff_v;
-}
-
-double LevelSet::diff_m_v(const std::vector<std::vector<double>>& F, int index, int x, int y) const {
-    if(index==0) { //  x
-        if(x>0)
-            return 1.0/1 * (F[x][y]-F[x-1][y]);
-        else
-            return 0.0; // 1.0/1 * (F(x+1,y,z)-F(x,y,z));
-    }
-    else if (index==1) { // y
-        if(y>0)
-            return 1.0/1.0 * (F[x][y]-F[x][y-1]);
-        else
-            return 0.0; // 1.0/1.0 * (F(x,y+1,z)-F(x,y,z));
-    }
-    // NOT REACHED
-    return 0.0;
-}
-
-/*
- * diff_p_v:
- *      dérivé à droite (p <-> plus)
- *
- */
-std::vector<std::vector<double>> LevelSet::diff_p_v(const std::vector<std::vector<double>>& F, int index) const
-{
-    std::vector<std::vector<double>> diff_v = F;
-    for (int i=0; i<diff_v.size(); i++)
-    {
-      for(int j=0; j<diff_v[0].size(); j++ )
-      {
-        diff_v[i][j] = 0.;
-      }
-    }
-
-    for (int  i=0; i<F.size(); ++i)
-        for (int  j=0; j<F[0].size(); ++j)
-            diff_v[i][j] = diff_p_v(F, index, i, j);
-    return diff_v;
-}
-
-double LevelSet::diff_p_v(const std::vector<std::vector<double>>& F, int index, int x, int y) const
-{
-    if (index==0) { //  x
-        if(x<F.size()-1)
-            return 1.0/1 * (F[x+1][y]-F[x][y]);
-        else
-            return 0.0; // 1.0/1 * (F(x,y,z)-F(x-1,y,z));
-    }
-    else if (index==1){ // y
-        if(y<F[0].size()-1)
-            return 1.0/1.0 * (F[x][y+1]-F[x][y]);
-        else
-            return 0.0; // 1.0/1.0 * (F(x,y,z)-F(x,y-1,z));
-
-    }
-    // NOT REACHED
-    return 0.0;
-}
-
-//fonction max min
-// double fmax(std::vector<double> v)
-// {
-//   double max_v(v[0]);
-//   for (size_t i = 1; i < v.size(); i++) {
-//     if ( v[i]> max_v) {max_v = v[i];};
-//   }
-//   return max_v;
-// }
-
-double fmax(const std::vector<std::vector<double>> v)
-{
-  double max_v(v[0][0]);
-  for (size_t i = 1; i < v.size(); i++) {
-    for (size_t j = 0; j < v[0].size(); j++) {
-      if ( v[i][j]> max_v) {max_v = v[i][j];};
-    }
-  }
-  return max_v;
-}
-
-// double fmin(std::vector<double> v)
-// {
-//   double min_v(v[0]);
-//   for (size_t i = 1; i < v.size(); i++) {
-//     if ( v[i]< min_v) {min_v = v[i];};
-//   }
-//   return min_v;
-// }
-
-double fmin(std::vector<std::vector<double>> v)
-{
-  double min_v(v[0][0]);
-  for (size_t i = 1; i < v.size(); i++) {
-    for (size_t j = 0; j < v[0].size(); j++) {
-      if ( v[i][j]< min_v) {min_v = v[i][j];};
-    }
-  }
-  return min_v;
 }
 
 /*
@@ -598,7 +306,5 @@ double LevelSet::diff_p(const field& F, int index, int x, int y) const
     // NOT REACHED
     return 0.0;
 }
-
-
 
 #endif
--- mainSegmentation.cc
+++ mainSegmentation.cc
@@ -10,6 +10,7 @@
 #include "InitMask.h"
 #include "ChanVeseSchemes.h"
 #include "Util.h"
+#include "LevelSet_v.h"
 
 using namespace std;
 
@@ -81,7 +82,7 @@ int main(int argc, char** argv)
   std::vector< std::vector <double> > newphi_v;
 
   newphi_v.resize(phi.rows());
-  for (int i=0;i<newphi_v ;i++) { phi_v[i].resize(phi.cols()); }
+  for (int i=0;i<newphi_v.size() ;i++) { phi_v[i].resize(phi.cols()); }
 
   std::string scheme(c.scheme);
 
@@ -89,54 +90,57 @@ int main(int argc, char** argv)
   {
     std::cout << "Explicit scheme" << std::endl;
     newphi_v = chanVese->ExplicitScheme(phi_v,c.dt,c.mu,c.nu,c.l1,c.l2);
-// CL
-    int nx= phi.size();
-    int ny= phi[0].size();
 
-    for (int j=0; j<ny; ++j)
-    {
-      newphi[0][j]  = newphi[1][j];
-      newphi[nx][j] = newphi[nx-1][j];
-    }
-
-    for (int i=0; i<nx; ++i)
-    {
-      newphi[i][0]  = newphi[i][1];
-      newphi[i][ny] = newphi[i][ny-1];
-    }
-// Fin CL
   }
   else
   {
     std::cout << "Seulement le schéma ExplicitScheme est implémenté." << std::endl;
   }
-  double diff = chanVese->fdiff(newphi_v);
+  double diff = chanVese->fdiff(phi_v, newphi_v);
   // double diff = (((newphi>=0).cast<double>()-0.5)*2. - ((phi>=0).cast<double>()-0.5)*2.).matrix().norm()
-                // /(phi.rows()*phi.cols());
-  phi = newphi;
+  // /(phi.rows()*phi.cols());
+  phi_v = newphi_v;
   int i(2);
   while ( (diff > 5e-6) && (i < 100) )
   {
     if (i%10 == 0) { std::cout << "Iteration -- " << i << std::endl;}
     if (scheme == "ExplicitScheme")
     {
-      newphi = chanVese->ExplicitScheme(phi,c.dt,c.mu,c.nu,c.l1,c.l2);
+      newphi_v = chanVese->ExplicitScheme(phi_v,c.dt,c.mu,c.nu,c.l1,c.l2);
 
-    diff = chanVese->fdiff(newphi_v);
-    // diff = (((newphi>=0).cast<double>()-0.5)*2. - ((phi>=0).cast<double>()-0.5)*2.).matrix().norm()
-                  // /(phi.rows()*phi.cols());
-    if (i%10 == 0)
-    {
-      newphi = ((newphi>=0).cast<double>()-0.5)*2;
-      LevelSet lv(newphi);
-      cout << "Redistanciation... " << endl;
-      lv.redistancing(10);
-      saveVTKFile(newphi, ("Results/sol_" + to_string(i)+ ".vtk").c_str());
-      std::cout << "Evolution of phi : " << diff << std::endl;
+      diff = chanVese->fdiff(phi_v, newphi_v);
+      // diff = (((newphi>=0).cast<double>()-0.5)*2. - ((phi>=0).cast<double>()-0.5)*2.).matrix().norm()
+      // /(phi.rows()*phi.cols());
+      if (i%10 == 0)
+      {
+        // newphi = ((newphi>=0).cast<double>()-0.5)*2;
+        for (int i=0 ; i < newphi_v.size(); i++)
+        {
+          for (int j=0 ; j < newphi_v.size(); j++)
+          {
+            newphi_v[i][j]=newphi_v[i][j]/abs(newphi_v[i][j]);
+          }
+        }
+        LevelSet_v lv_v(newphi_v);
+        cout << "Redistanciation... " << endl;
+        lv_v.redistancing_v(10);
+        saveVTKFile(newphi_v, ("Results/sol_" + to_string(i)+ ".vtk").c_str());
+        std::cout << "Evolution of phi : " << diff << std::endl;
+      }
+      phi_v = newphi_v;
+      i++;
     }
-    phi = newphi;
-    i++;
   }
+
+  field newphi;
+  for (int i=0 ; i < phi.rows(); i++)
+  {
+    for (int j=0 ; j < phi.cols(); j++)
+    {
+      newphi(i,j) = phi_v[i][j];
+    }
+  }
+
   newphi = ((newphi>=0).cast<double>()-0.5)*2;
   saveVTKFile(newphi, "Results/lastsol.vtk");
   image->WriteImage(chanVese->AbsGradPhi(newphi), (namewithoutextension + "_filtered_with_contour." + extension).c_str());
@@ -145,3 +149,20 @@ int main(int argc, char** argv)
 
   return 0;
 }
+
+// // CL
+// int nx= phi_v.size();
+// int ny= phi_v[0].size();
+//
+// for (int j=0; j<ny; ++j)
+// {
+//   newphi[0][j]  = newphi[1][j];
+//   newphi[nx][j] = newphi[nx-1][j];
+// }
+//
+// for (int i=0; i<nx; ++i)
+// {
+//   newphi[i][0]  = newphi[i][1];
+//   newphi[i][ny] = newphi[i][ny-1];
+// }
+// // Fin CL
